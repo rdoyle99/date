c321624710f624d82f48ed5107f7cc973d271f25
rdoyle99 <rpdoyle1@gmail.com>
2025-08-20 09:04:37 +0800

    webp

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ const storage = multer.diskStorage({
 const upload = multer({ 
   storage: storage,
   fileFilter: (req, file, cb) => {
-    const allowedTypes = /jpeg|jpg|png|gif/;
+    const allowedTypes = /jpeg|jpg|png|gif|webp/;
     const extname = allowedTypes.test(path.extname(file.originalname).toLowerCase());
     const mimetype = allowedTypes.test(file.mimetype);
     
